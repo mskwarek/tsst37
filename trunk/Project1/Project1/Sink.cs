7dbdcad78d60ecd391f482246f8bba372675a186
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-04 21:13:54 +0000

    Sink.cs

---
+++ trunk/Project1/Project1/Sink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+
+    /*Urządzenie odbierające ze swojego portu ruch sieciowy 
+ * 
+ * 
+ */
+    class Sink
+    {
+
+        private PortIn lastport;
+
+
+
+
+
+
+
+    }
+}
