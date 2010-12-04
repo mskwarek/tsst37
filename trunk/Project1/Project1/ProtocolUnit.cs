eeb0b0806fdfddecd2af26169e49b5d8ea681abe
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-04 17:45:43 +0000

    ProtocolUnit.cs

--- trunk/Project1/Project1/ProtocolUnit.cs
+++ trunk/Project1/Project1/ProtocolUnit.cs
@@ -6,6 +6,39 @@ using System.Text;
 namespace Project1
 {
     class ProtocolUnit
-    {
+    {   //czyli juz dataunit z numerami vci vpi
+
+        private int vpi;
+
+        private int vci;
+
+        private DataUnit datau;
+
+        private Random rand = new Random();  //do generowania vci vpi randomowych..pewnie bedzie nie potrzebne ale napisalem
+
+
+
+        public int GetVCI()
+        { return vci; }
+
+        public void SetVCI(int vci)
+        { this.vci = vci; }
+
+        public int SetRandomVCI()
+        { return this.vci = (int)rand.Next((int)Math.Pow(2, 16)); }
+
+        public int GetVPI()
+        { return vpi; }
+
+        public void SetVPI(int vpi)
+        { this.vpi = vpi; }
+
+        public int SetRandomVPI()
+        { return this.vpi = (int)rand.Next((int)Math.Pow(2, 12)); }
+
+        public DataUnit GetDataUnit() { return datau; }
+
+        public void SetDataUnit(DataUnit d) { datau = d; }
+
     }
 }
