4d97c89e7242139964d0b41a51779ceb9a6a38bc
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-04 20:37:05 +0000

    PortOut.cs

--- Project1/Project1/PortOut.cs
+++ Project1/Project1/PortOut.cs
@@ -6,6 +6,17 @@ using System.Text;
 namespace Project1
 {
     class PortOut
-    {
+    {        // reprezancacja portu wyjściowego, realizuje przekazanie danych do portu po drugiej stronie łącza 
+
+
+        private int number; //numer identyfikujacy port wyjsciowy.
+
+        public int GetNumber() { return number; }
+
+        public void SetNumber(int i) { number = i; }
+
+        public void Send(ProtocolUnit pu) { }  // metoda ktora bedzie wysylala ProtocolUnit ktory jej podamy na wejscie.
+
+
     }
 }
