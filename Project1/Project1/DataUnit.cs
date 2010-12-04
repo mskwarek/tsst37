e0c4c451ae1267245707e5f42a40e844d536f1f8
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-04 17:31:46 +0000

    dataunit

--- Project1/Project1/DataUnit.cs
+++ Project1/Project1/DataUnit.cs
@@ -6,6 +6,15 @@ using System.Text;
 namespace Project1
 {
     class DataUnit
-    {
+    {   // reprezentuje 48B kawalek Data
+
+        private string id;  // czyli litera duza symbolizujaca ta wiadomosc 48B
+
+        public string GetId() { return id; }
+
+        public string GetId(Data d, int element) { return d.GetElement(element); }  //tu pobieramy wybrany kawalek data 
+
+        public void SetId(string s) { id = s; }
+
     }
 }
