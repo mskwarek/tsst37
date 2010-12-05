2ab5ff66f1b00ec9a42084d8c749f6f5797ce70b
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-05 20:00:20 +0000

    Serial - klasa do zamiany object na xmlstring CreateHost klasa odpowiedzialna za tworzenie hosta IPort interfejssc portu Component klasa z ktora zawiera informacje o node jego porty wejsciowe i wyjsciowe do wczytania w klasie port

---
+++ trunk/Project1/Project1/IPort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Project1
+{   [ServiceContract]
+    public interface IPort
+    {
+    [OperationContract]
+    void Recieve(int idPortu, string message);
+
+
+    }
+}
