88320c4ce1d906a6cdf7b11ea73f03bdb3f2c651
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-05 20:00:20 +0000

    Serial - klasa do zamiany object na xmlstring CreateHost klasa odpowiedzialna za tworzenie hosta IPort interfejssc portu Component klasa z ktora zawiera informacje o node jego porty wejsciowe i wyjsciowe do wczytania w klasie port

---
+++ Project1/Project1/Component.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace Project1
+{
+    [XmlRootAttribute("Component", Namespace = "", IsNullable = false)]
+    public class Component
+    {
+        [XmlElementAttribute("Id")]
+        public int id { get; set; }
+        [XmlArrayItem(typeof(Port))]
+        public List<PortType> list {get; set;}
+
+        public Component() {list = new List<PortType>(); }
+    }
+    [Serializable]
+    public class PortType 
+    {   
+       [XmlElementAttribute("Type")]
+        public String type{ get; set; }
+        [XmlElementAttribute("Link")]
+       public int link { get; set; }
+    }
+ 
+}
