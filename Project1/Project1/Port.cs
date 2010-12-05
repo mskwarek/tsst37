4b8afb688ff6a315f9fab93e2f4dc764d580d9f0
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-05 20:02:55 +0000

    klasa ktora bedzie odbierac informacje i przekazywac je do PortIn

---
+++ Project1/Project1/Port.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Project1
+{
+   public class Port : IPort
+    {
+        public static string messagez = null;
+        public static  bool isOcuppiedz = false;
+        public static Component component;
+        public static Node node;
+        public Port()
+        {
+         
+        }
+        public void Recieve(int idPortu, string message)
+        {
+           
+                messagez = message;
+                isOcuppiedz = true;
+
+
+            }
+                
+        
+
+      
+    }
+}
