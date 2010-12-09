fd238f68f7318d3175a606e458d226d590fac76b
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-09 01:38:28 +0000

    Node.cs

--- trunk/Project1/Project1/Node.cs
+++ trunk/Project1/Project1/Node.cs
@@ -5,17 +5,18 @@ using System.Text;
 
 namespace Project1
 {
-
-    class Node
+    public class Node
     {    //pojedynczy węzeł sieci, nie generuje ruchu, tylko go kieruje
 
-        private string name;
+        public Agent a;
 
-        private PortIn[] inportsgroup;  //porty wejsciowe wezla
+        private string name = "unknown";
+
+        private PortIn[] inportsgroup; //porty wejsciowe wezla
 
         private PortOut[] outportsgroup; //porty wyjsciowe wezla
 
-        private Matrix tab;  //Matrix wezla czyli tablica routingu
+        private Matrix tab = new Matrix();  //Matrix wezla czyli tablica routingu
 
         public string GetName() { return name; }   //zwraca imie wezla
 
@@ -34,6 +35,29 @@ namespace Project1
         public void SetMatrix(Matrix m) { tab = m; } //Ustawienie pola kom. dla węzla
 
 
+        public Node(int numberofin, int numberofout, string name)
+        {
+
+            this.name = name;
+
+            inportsgroup = new PortIn[numberofin];
+            outportsgroup = new PortOut[numberofout];
+
+            for (int i = 0; i < inportsgroup.Length; i++) { inportsgroup[i] = new PortIn(); }
+            for (int j = 0; j < outportsgroup.Length; j++) { outportsgroup[j] = new PortOut(); }
+
+
+
+
+
+
+            this.a = new Agent(this);
+        }
+
+
+
+
+
         /*
          Metoda ChecInPorts najpierw poszukuje w puli portow wejsciowych ktore naleza do wezla tego na ktory wszedl pakiet .
          * Nastepnie szukamy odpowiadajacego temu pakietowi klucza w Tablicy Routingu. Klucz ten to  "numerInPort:wejscioweVPI:wejscioweVCI".
@@ -48,9 +72,9 @@ namespace Project1
         {
 
             // tym trzem zmiennym bedziemy przypisywac numer portu wyjsciowego,vpi i vci wyjsciowe
-            int portnum = new int();
-            int vpi = new int();
-            int vci = new int();
+            ///int portnum=new int();
+            /// int vpi=new int();
+            /// int vci=new int();
 
 
             foreach (PortIn p in inportsgroup)
@@ -58,23 +82,23 @@ namespace Project1
 
                 if (p.GetIsReceived() == true)
                 {    //przechodzimy przez ten if gdy znajdziemy port z pakietem do obsluzenia               
-
+                    MatrixElements me = new MatrixElements(p.GetNumber(), p.GetProtocolUnit().GetVPI(), p.GetProtocolUnit().GetVCI());
                     foreach (DictionaryEntry de in tab.GetRouteTable())//przeszukujemy tablice routingu..
                     {
                         //az napotkamy wpis odpowiadajacy kluczowi utworzonemu z vci,vpi pakietu i z numeru portu wejsciowego.(wtedy mamy if(true))
-                        if ((string)de.Key == p.GetNumber().ToString() + ":" + p.GetProtocolUnit().GetVPI().ToString() + ":" + p.GetProtocolUnit().GetVCI().ToString())
+                        if (((MatrixElements)de.Key).Equals(me))
                         {
-                            string s = (string)de.Value;//odczytujemy wartosc tablicy routingu 
+                            me = (MatrixElements)de.Value;//odczytujemy wartosc tablicy routingu 
 
 
                             //w ponizszych trzech wierszach otrzymujemy vci,vpi i numer portu wyjsciowego w postaci int
-                            portnum = int.Parse(s.Substring(0, s.IndexOf(":")));
-                            vpi = int.Parse(s.Substring(s.IndexOf(":") + 1, s.LastIndexOf(":") - s.IndexOf(":") - 1));
-                            vci = int.Parse(s.Substring(s.LastIndexOf(":") + 1, s.Length - s.LastIndexOf(":") - 1));
+                            ///portnum = int.Parse(s.Substring(0, s.IndexOf(":")));
+                            /// vpi = int.Parse(s.Substring(s.IndexOf(":") + 1, s.LastIndexOf(":") - s.IndexOf(":") - 1));
+                            ///vci = int.Parse(s.Substring(s.LastIndexOf(":") + 1, s.Length - s.LastIndexOf(":") - 1));
 
                             // wpisujemy w pakiecie ktory znalezlismy w jednym z portow wejsciowych nowe vci i vpi z tablicy routingu
-                            p.GetProtocolUnit().SetVPI(vpi);
-                            p.GetProtocolUnit().SetVCI(vci);
+                            p.GetProtocolUnit().SetVPI(me.GetVPI());
+                            p.GetProtocolUnit().SetVCI(me.GetVCI());
 
                             break;   //zeby nie przechodzic juz przez tablice jak znajdziemy wpis
                         }
@@ -85,7 +109,7 @@ namespace Project1
                     // W ponizszej petli szukamy w puli portow wyjsciowych wezla port ktorym mamy wyslac pakiet i go wysylamy
                     foreach (PortOut pp in outportsgroup)
                     {
-                        if (pp.GetNumber() == portnum)
+                        if (pp.GetNumber() == me.GetPortNumber())
                         { pp.Send(p.GetProtocolUnit()); }
                     }
 
@@ -100,5 +124,4 @@ namespace Project1
 
 
     }
-
 }
