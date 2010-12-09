29b6687c62800a119a9659708d05b2312952b0d9
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-09 13:26:56 +0000

    Component z funkcjonalnoscia umozliwiajaca konwersje Component to Node

--- Project1/Project1/Component.cs
+++ Project1/Project1/Component.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.IO;
 using System.Xml.Serialization;
 using System.Xml;
-
+//klasa component sluzy to archiwizowania wlasciwowsci zawartych w obiekcie ppodstawowym Node
 namespace Project1
 {
     [XmlRootAttribute("Component", Namespace = "", IsNullable = false)]
@@ -17,6 +17,26 @@ namespace Project1
         public List<PortType> list {get; set;}
 
         public Component() {list = new List<PortType>(); }
+
+        //metoda przerabiajaca component na element Node
+       public  Node getNode()
+        {
+       
+            List<PortIn> portIn =new List<PortIn>();
+            List<PortOut> portOut = new List<PortOut>();
+            foreach(PortType p in list)
+            {
+                if (p.type == "Project1.PortIn")
+                    portIn.Add(new PortIn(p.portNumber));
+                if (p.type == "Project1.PortOut")
+                    portOut.Add(new PortOut(p.portNumber));
+            }
+            Node node = new Node(portIn.Count, portOut.Count, "Node number" + id);
+            node.SetPortsIn(portIn.ToArray());
+            node.SetPortsOut(portOut.ToArray());
+            return node;
+           
+        }
     }
     [Serializable]
     public class PortType 
