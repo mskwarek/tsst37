00bab524552489d554aad27df4bd01959e91adbe
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-09 14:16:48 +0000

    component poprawiony tym razem wczytuje tez sink i source todo: jak sink bedzie zrobione trzeba umozliwic tworzenie takich obiektow

--- trunk/Project1/Project1/Component.cs
+++ trunk/Project1/Project1/Component.cs
@@ -13,29 +13,73 @@ namespace Project1
     {
         [XmlElementAttribute("Id")]
         public int id { get; set; }
+        [XmlElementAttribute("NodeType")]
+        public string nodeType { get; set; }
+
         [XmlArrayItem("PortType")]
         public List<PortType> list {get; set;}
 
         public Component() {list = new List<PortType>(); }
 
         //metoda przerabiajaca component na element Node
-       public  Node getNode()
+       public  Object getNode()
         {
-       
-            List<PortIn> portIn =new List<PortIn>();
-            List<PortOut> portOut = new List<PortOut>();
-            foreach(PortType p in list)
+
+            if (nodeType.ToLower() == "node")
             {
-                if (p.type == "Project1.PortIn")
-                    portIn.Add(new PortIn(p.portNumber));
-                if (p.type == "Project1.PortOut")
-                    portOut.Add(new PortOut(p.portNumber));
+                List<PortIn> portIn = new List<PortIn>();
+                List<PortOut> portOut = new List<PortOut>();
+                foreach (PortType p in list)
+                {
+                    if (p.type == "Project1.PortIn")
+                        portIn.Add(new PortIn(p.portNumber));
+                    if (p.type == "Project1.PortOut")
+                        portOut.Add(new PortOut(p.portNumber));
+                }
+                Node node = new Node(portIn.Count, portOut.Count, "Node number" + id);
+                node.SetPortsIn(portIn.ToArray());
+                node.SetPortsOut(portOut.ToArray());
+                return (Node)node;
             }
-            Node node = new Node(portIn.Count, portOut.Count, "Node number" + id);
-            node.SetPortsIn(portIn.ToArray());
-            node.SetPortsOut(portOut.ToArray());
-            return node;
-           
+
+            switch (nodeType.ToLower())
+            {
+                case "node":
+                    List<PortIn> portIn = new List<PortIn>();
+                    List<PortOut> portOut = new List<PortOut>();
+                    foreach (PortType p in list)
+                    {
+                        if (p.type == "Project1.PortIn")
+                            portIn.Add(new PortIn(p.portNumber));
+                        if (p.type == "Project1.PortOut")
+                            portOut.Add(new PortOut(p.portNumber));
+                    }
+                    Node node = new Node(portIn.Count, portOut.Count, "Node number" + id);
+                    node.SetPortsIn(portIn.ToArray());
+                    node.SetPortsOut(portOut.ToArray());
+                    return (Node)node;
+                   
+                case "source":
+                    Sorce source = new Sorce();
+                    if (list.Count == 1 && list[0].type == "Project1.PortOut")
+                    {
+                        source.SetPortOut(new PortOut(list[0].portNumber));
+                        return (Sorce)source;
+                    }
+                    else return null;
+                 case "sink":
+                    Sink sink = new Sink();
+                   if (list.Count == 1 && list[0].type == "Project1.PortIn")
+                    {
+                    //    source.SetPortOut(new PortOut(list[0].portNumber));
+                       return (Sink)sink;
+                    }
+                   else return null;
+                   
+                default:
+                    return null;
+                    
+            }
         }
     }
     [Serializable]
