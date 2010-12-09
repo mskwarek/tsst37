8b8ba277efa98cdcc8527569a4a7321788a8a176
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-09 01:35:41 +0000

    MatrixElements.cs

---
+++ Project1/Project1/MatrixElements.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+    public class MatrixElements //: IEquatable<MatrixElements>
+    {
+        private int portnum;
+
+        private int vpi;
+
+        private int vci;
+
+
+
+        public MatrixElements(int portnum, int vpi, int vci)
+        {
+            this.vpi = vpi;
+            this.vci = vci;
+            this.portnum = portnum;
+        }
+
+        public bool Equals(MatrixElements me)
+        {
+            if (me.portnum == portnum && me.vpi == vpi && me.vci == vci)
+                return true;
+            else
+                return false;
+
+
+        }
+        public int GetPortNumber() { return portnum; }
+
+        public int GetVPI() { return vpi; }
+
+        public int GetVCI() { return vci; }
+
+        public void SetPortNumber(int portnum) { this.portnum = portnum; }
+
+        public void SetVPI(int vpi) { this.vpi = vpi; }
+
+        public void SetVCI(int vci) { this.vci = vci; }
+
+    }
+}
