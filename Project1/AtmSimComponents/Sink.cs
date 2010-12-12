403497e7799e138b075670a4729e6212f3958054
grzesiek.net <grzesiek.net@95eec99f-bba9-b184-8604-941c0c345358>
2010-12-12 16:29:28 +0000

    Sink

--- Project1/AtmSimComponents/Sink.cs
+++ Project1/AtmSimComponents/Sink.cs
@@ -10,26 +10,59 @@ namespace AtmSim.Components
  * 
  * 
  */
-    public class Sink// : AtmSim.Common.INetworkNode
+    public class Sink : AtmSim.Common.INetworkNode
     {
-        class SinkReceiver : IFrameReceiver
-        {
-            public void ReceiveFrame(ProtocolUnit pu, int port)
-            {
+        
 
+        public string Name;
+        private Common.Log log;
+        public Common.Log Log { get { return log; } set { log = value; } }
+
+        private SinkAgent agent;
+        public Common.IAgent Agent
+        {
+            get { return agent; }
+        }
+
+        private IPortIn firstport;
+        private string message = "";
+        private string target = "";
+        public string Message { get { return message; } set { message = value; } }
+        public string Target { get { return target; } set { target = value; } }
+       public  class SinkReceiver : IFrameReceiver
+        {   private Sink sink;
+            private Common.RoutingTable RouteTable = new Common.RoutingTable();
+            public Common.RoutingTable GetRouteTable() { return RouteTable; }
+            public SinkReceiver(Sink sink)
+                 {
+                 this.sink = sink;
+                 }
+            public void ReceiveFrame(ProtocolUnit pu, int port)
+            {   
+                Common.RoutingEntry source = new Common.RoutingEntry(port, pu.Vpi, pu.Vci);
+                if (RouteTable.ContainsKey(source.ToString()))
+                {
+                   
+                   sink.Log.LogMsg("Ramka: " + source.ToString());
+                }
             }
         }
 
         
 
-        private SinkReceiver receiver;
-        private TestPortIn lastport;
+        public SinkReceiver receiver;
 
-        public Sink(TestPortIn port)
+        public void SetPortIn(IPortIn po) { firstport = po; } //skojarzenie Sorcea z odpowiednim portem wyjsciowym
+
+        public IPortIn GetPortIn() { return firstport; }
+    
+
+        public Sink(String name)
         {
-            receiver = new SinkReceiver();
-            lastport = port;
-            lastport.SetReceiver(receiver);
+            this.Name = name;
+            this.log = new Common.Log("Log źródła " + name);
+            agent = new SinkAgent(this);
+        
         }
     }
 }
