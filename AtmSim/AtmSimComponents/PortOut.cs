5d6e6512f9f2b2f85283ac979eeb99a9c291239e
sajkolpl <sajkolpl@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-15 18:15:20 +0000

    Przepisany PortOut (nie używane w programie, nie testowane, kompilowalne)

--- AtmSim/AtmSimComponents/PortOut.cs
+++ AtmSim/AtmSimComponents/PortOut.cs
@@ -8,100 +8,35 @@ using System.Threading;
 
 namespace AtmSim.Components
 {
-   public class PortOut
-   {
-       private static int counting = 0;
-
-       public PortOut() { number = counting; counting++; }
-
-       public int GetNumber() { return number; }
-
-       public void SetNumber(int i) { number = i; }
-
-       public int GetCounting() { return counting; }
-       // reprezancacja portu wyjściowego, realizuje przekazanie danych do portu po drugiej stronie łącza 
-       public bool isOccupied;
-
-       public ProtocolUnit protuni{get; set;}
-
-        private int number=0; //numer identyfikujacy port wyjsciowy.
-
-        public void Send(ProtocolUnit pu) { }  // metoda ktora bedzie wysylala ProtocolUnit ktory jej podamy na wejscie.
-        
-       private Int32 realPort;
-
-       public PortOut(Int32 realPort)
-       {
-        this.realPort = realPort;
-        isOccupied = false;
-       }
-       
-       private  void Connect(String server, String message)
+    public class PortOut : IPortOut
+    {
+        int _portID;
+        public int portID
         {
-            try
-            {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
+            get { return _portID; }
+            set { _portID = value; }
+        }
+        private Socket remoteSocket;
+        //private byte[] buffer = new byte[1024];
+        private int _tcpPort;
+        public int tcpPort
+        {
+            get { return _tcpPort; }
+            set { _tcpPort = value; }
+        }
+        private bool _open = false;
+        public bool isOpen { get { return _open; } }
+        private bool _connected = false;
+        public bool isConnected { get { return _connected; } }
 
-                TcpClient client = new TcpClient(server, realPort);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("PortOut send: {0}\n", message);
-
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}\n", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-
-           // Console.WriteLine("\n Press Enter to continue...");
-           // Console.Read();
+        public void Send(string pu)
+        {
+            throw new NotImplementedException();
         }
 
-
-       public void listener()
-       {
-            while (true)
-            {
-                if (this.isOccupied == true)
-                {
-                    this.Connect("127.0.0.1", (string)Serial.SerializeObject(this.protuni));
-                    this.isOccupied = false;
-
-                }
-            }
+        public void Send(ProtocolUnit pu)
+        {
+            throw new NotImplementedException();
         }
     }   
 }
