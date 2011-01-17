300c6131f94b037a6aaa426f238150162aa28edf
piotr.lonc <piotr.lonc@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-17 15:44:10 +0000

    pro2

---
+++ Node.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace AtmSim.Components
+{
+    public class Node : INetworkNode
+    {    //pojedynczy węzeł sieci, nie generuje ruchu, tylko go kieruje
+        private NodeAgent a;
+        public IAgent Agent
+        {
+            get { return a; }
+        }
+
+        private Log log;
+        public Log Log
+        {
+            get { return log; }
+        }
+
+        private string name = "unknown";
+        private int id;
+
+        private PortIn[] inportsgroup; //porty wejsciowe wezla
+        public PortIn[] PortsIn { get { return inportsgroup; } }
+        private PortOut[] outportsgroup; //porty wyjsciowe wezla
+        public PortOut[] PortsOut { get { return outportsgroup; } }
+        private Matrix tab; //= new Matrix();  //Matrix wezla czyli tablica routingu
+        public Matrix Matrix { get { return tab; } }
+
+        
+        public string Name { get { return name; } set { name = value; } }
+        public int Id { get { return id; } set { id = value; } }
+ 
+ //       public IPortIn[] GetPortsIn() { return inportsgroup; } //metoda zwraca porty wejsciowe
+ //       public IPortOut[] GetPortsOut() { return outportsgroup; }  //metoda zwraca porty wyjsciowe
+
+ //       public void SetPortsIn(IPortIn[] pi) { inportsgroup = pi; } //metoda ustawia porty wejsciowe
+ //       public void SetPortsOut(IPortOut[] po) { outportsgroup = po; } //metoda ustawia porty wyjsciowe
+
+ //       public Matrix GetMatrix() { return tab; }  //zwraca Matrix-pole komutacyjne węzla
+ //       public void SetMatrix(Matrix m) { tab = m; } //Ustawienie pola kom. dla węzla
+
+        public Node(int numberofin, int numberofout, int id, string name, int managerPort)
+        {
+            this.id = id;
+            this.name = name;
+
+            inportsgroup = new PortIn[numberofin];
+            outportsgroup = new PortOut[numberofout];
+            tab = new Matrix(this);
+
+            for (int i = 0; i < inportsgroup.Length; i++)
+            {
+                PortIn port = new PortIn(i);
+                port.SetReceiver(this.tab);
+                inportsgroup[i] = port;
+            }
+            for (int j = 0; j < outportsgroup.Length; j++) 
+            {
+                outportsgroup[j] = new PortOut(j);
+            }
+
+            this.log = new Log("Log urzadzenia " + name);
+            this.a = new NodeAgent(this, managerPort);
+        }
+    }
+}
