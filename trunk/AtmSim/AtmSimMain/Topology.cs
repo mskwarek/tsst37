d35de7361a12a8fe19de3d81413f2282f698177b
sajkolpl <sajkolpl@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-18 01:29:08 +0000

    Rysowanie topologii dopasowane do nowego systemu

---
+++ trunk/AtmSim/AtmSimMain/Topology.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickGraph;
+using GraphSharp.Controls;
+
+namespace AtmSim
+{
+    public class Topology : BidirectionalGraph<Topology.Node, Topology.Link>
+    {
+        public class Node
+        {
+            public int Id;
+            public string Name;
+
+            public Node(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        public class Link : Edge<Node>
+        {
+            public string Tag { get; private set; }
+            public Link(string tag, Node source, Node target)
+                : base(source, target)
+            {
+                Tag = tag;
+            }
+        }
+
+        public Topology() { }
+
+        public Topology(bool allowParallelEdges)
+            : base(allowParallelEdges) { }
+
+        public Topology(bool allowParallelEdges, int vertexCapacity)
+            : base(allowParallelEdges, vertexCapacity) { }
+    }
+
+}
