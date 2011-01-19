e0336638dcf64f49de794d6c8caf73a7f8879be8
sajkolpl <sajkolpl@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-19 18:17:45 +0000

    Dodany proces Sink

--- AtmSim/AtmSimNode/SinkForm.cs
+++ AtmSim/AtmSimNode/SinkForm.cs
@@ -11,8 +11,10 @@ namespace AtmSim
 {
     public partial class SinkForm : Form
     {
+        private Components.Sink sink;
         public SinkForm(Config.Node cNode, int mPort)
         {
+            sink = new Components.Sink(cNode, mPort);
             InitializeComponent();
         }
     }
