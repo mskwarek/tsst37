dda3265cc716a271e6223dc6e33e1cafabe65e43
sajkolpl <sajkolpl@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-21 00:00:13 +0000

    Klasa NetworkConnection do przechowywania informacji o połączeniu

---
+++ AtmSim/AtmSimMain/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtmSim
+{
+    class LinkConnection
+    {
+        public int SourceId;
+        public int TargetId;
+        public string SourceRouting;
+        public string TargetRouting;
+    }
+
+    class NetworkConnection
+    {
+        public int Id { get; private set; }
+        public List<LinkConnection> Path;
+        public bool Active;
+    }
+}
