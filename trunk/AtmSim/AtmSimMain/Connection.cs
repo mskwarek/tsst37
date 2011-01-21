83ea63aa6e079bc701536626ae8cc0b28c7990dc
sajkolpl <sajkolpl@95eec99f-bba9-b184-8604-941c0c345358>
2011-01-21 00:07:11 +0000

    uzupełnienie NetworkConnection

--- trunk/AtmSim/AtmSimMain/Connection.cs
+++ trunk/AtmSim/AtmSimMain/Connection.cs
@@ -9,14 +9,26 @@ namespace AtmSim
     {
         public int SourceId;
         public int TargetId;
-        public string SourceRouting;
-        public string TargetRouting;
+        public string SourceRouting; // Port:Vpi:Vci
+        public string TargetRouting; // Port:Vpi:Vci
     }
 
     class NetworkConnection
     {
         public int Id { get; private set; }
         public List<LinkConnection> Path;
+        public List<int> Nodes
+        {
+            get
+            {
+                List<int> nodes = new List<int>();
+                if (Path.Count > 0)
+                    nodes.Add(Path.First().SourceId);
+                foreach (var link in Path)
+                    nodes.Add(link.TargetId);
+                return nodes;
+            }
+        }
         public bool Active;
     }
 }
